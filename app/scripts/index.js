ce0813b7a7097aa9a2c6e7b396d3d3b6180256da
Jordan S <jpstanway@gmail.com>
2018-10-08 16:52:27 -0700

    create bar elements with placeholder data for gdp and year

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,16 +1,17 @@
 
 // get GDP/year data
 const dataset = [
-    [1, 1920],
-    [2, 1930],
-    [3, 1940],
-    [4, 1950],
-    [5, 1960],
-    [6, 1970],
-    [7, 1980],
-    [8, 1990],
-    [9, 2000],
-    [10, 2010]
+    [1910, 0],
+    [1920, 1],
+    [1930, 2],
+    [1940, 3],
+    [1950, 4],
+    [1960, 5],
+    [1970, 6],
+    [1980, 7],
+    [1990, 8],
+    [2000, 9],
+    [2010, 10]
 ];
 
 // set chart dimensions
@@ -20,11 +21,11 @@ const padding = 20;
 
 // set x and y scales
 const xScale = d3.scaleLinear()
-                .domain([0, d3.max(dataset, (d) => d[0])])
+                .domain([d3.min(dataset, (d) => d[0]), d3.max(dataset, (d) => d[0])])
                 .range([padding, w - padding]);
 
 const yScale = d3.scaleLinear()
-                .domain([d3.min(dataset, (d) => d[1]), d3.max(dataset, (d) => d[1])])
+                .domain([0, d3.max(dataset, (d) => d[1])])
                 .range([h - padding, padding]);
 
 // create svg area in container
@@ -32,6 +33,19 @@ const svg = d3.select('#container')
                 .append('svg')
                 .attr('width', w)
                 .attr('height', h);
+
+// create bar elements
+svg.selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('x', (d) => xScale(d[0]))
+    .attr('y', (d) => yScale(d[1]))
+    .attr('width', 25)
+    .attr('height', (d) => (h - padding) - yScale(d[1]))
+    .attr('class', 'bar')
+    .attr('data-gdp', (d) => d[0])
+    .attr('data-date', (d) => d[1]);               
 
 // set axes and append to DOM
 const xAxis = d3.axisBottom(xScale);
